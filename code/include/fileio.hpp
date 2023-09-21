165db4e9608a4477a2d2b83408f90a89beef7085
Bojan Endrovski <bojan.endrovski@gmail.com>
2023-09-21 16:07:04 +0200

    💽 file io and json config file

--- code/include/fileio.hpp
+++ code/include/fileio.hpp
@@ -7,4 +7,5 @@ namespace xxs::fileio
 {
 	std::vector<char> read_binary_file(const std::string& filename);
 	std::string read_text_file(const std::string& filename);
+	std::string get_path(const std::string& filename);
 }
--- code/source/main.cpp
+++ code/source/main.cpp
@@ -1,9 +1,33 @@
 #include "log.hpp"
+#include <string>
+#include <map>
+#include <json.hpp>
+#include <fstream>
+#include "fileio.hpp"
+
+struct config
+{
+    int width;
+    int height;
+    std::string title;
+};
+
+config parse_config(const std::string& config_str)
+{
+    config cfg;
+    nlohmann::json j = nlohmann::json::parse(config_str);
+    cfg.width = j["width"];
+    cfg.height = j["height"];
+    cfg.title = j["title"];
+    return cfg;
+}
 
 int main()
 {
-    xxs::log::info("Hello World{}", ".");
-    xxs::log::warn("Hello World{}", "?");
-    xxs::log::error("Hello World{}", "!");
+    xxs::log::info("xxs starting up");
+    auto config = xxs::fileio::read_text_file("config.json");
+    auto cfg = parse_config(config);
+    xxs::log::info("width: {}, height: {}, title: {}", cfg.width, cfg.height, cfg.title);
+
     return 0;
 }
