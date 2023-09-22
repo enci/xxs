3f13fa6581577cc681a1d046498c19d830dd0603
Bojan Endrovski <bojan.endrovski@gmail.com>
2023-09-22 14:02:15 +0200

    🪟 sdl window

--- code/source/main.cpp
+++ code/source/main.cpp
@@ -4,6 +4,7 @@
 #include <json.hpp>
 #include <fstream>
 #include "fileio.hpp"
+#include "SDL3/SDL.h"
 
 struct config
 {
@@ -27,7 +28,34 @@ int main()
     xxs::log::info("xxs starting up");
     auto config = xxs::fileio::read_text_file("config.json");
     auto cfg = parse_config(config);
-    xxs::log::info("width: {}, height: {}, title: {}", cfg.width, cfg.height, cfg.title);
+    xxs::log::info("width: {}, height: {}, title: {}",
+        cfg.width,
+        cfg.height,
+        cfg.title);
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow(
+        cfg.title.c_str(),       
+        cfg.width,
+        cfg.height,
+        SDL_WINDOW_OPENGL);
+
+    bool running = true;
+    while(running)
+    {
+        SDL_Event event;
+        while(SDL_PollEvent(&event))
+        {
+            switch(event.type)
+            {
+                case SDL_EVENT_QUIT:
+                    running = false;
+            }
+        }
+        
+        SDL_GL_SwapWindow(window);
+        SDL_Delay(1);
+    }
 
     return 0;
 }
